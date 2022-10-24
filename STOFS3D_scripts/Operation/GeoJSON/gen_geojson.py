b7989b6ac268a4bb8c54f673041d35f35d3aadea
cuill <cui86ll@gmail.com>
2022-10-24 10:07:47 -0400

    Update GeoJSON script

--- STOFS3D_scripts/Operation/GeoJSON/gen_geojson.py
+++ STOFS3D_scripts/Operation/GeoJSON/gen_geojson.py
@@ -14,27 +14,34 @@ from shapely.validation import make_valid
 from geopandas import GeoDataFrame
 
 #from STOFS3D_scripts.Utility import utils
+#from validation import make_valid
 import utils as utils
 
 def get_disturbance(elevation, depth, levels, fill_value, out_filename):
-
-    #set mask on dry nodes
-    idxs_dry = np.where(elevation + depth <= 1.e-6)
-    elevation[idxs] = fill_value
 
     #disturbance
     disturbance = copy.deepcopy(elevation)
     idxs_land_node = depth < 0
     disturbance[idxs_land_node] = np.maximum(0, elevation[idxs_land_node] + depth[idxs_land_node])
 
-    #set mask on dry nodes and nodes with small max disturbance (<0.3 m) on land
+    #set mask on dry nodes
+    idxs_dry = np.where(elevation + depth <= 1.e-6)
+    disturbance[idxs_dry] = fill_value
+
+    #set mask on nodes with small max disturbance (<0.3 m) on land
     idxs_small = disturbance < 0.3
     idxs_mask_maxdist = idxs_small * idxs_land_node
     disturbance[idxs_mask_maxdist] = fill_value
 
+    #get mask for triangulation
+    imask = disturbance < -90000
+    mask = np.any(np.where(imask[triangulation.triangles], True, False), axis=1)
+    triangulation.set_mask(mask)
     gdf = contour_to_gdf(disturbance, levels, triangulation)
 
-    gdf.to_file(out_filename, driver="GeoJSON")
+    #gdf.to_file(out_filename, driver="GeoJSON")
+    gdf.to_file(out_filename, driver="GPKG", layer='disturbance')
+    #gdf.to_file(out_filename)
 
 
 def contour_to_gdf(disturbance, levels, triangulation):
@@ -42,33 +49,24 @@ def contour_to_gdf(disturbance, levels, triangulation):
     MinVal = levels[0]
     MaxVal = levels[-1]
 
-    #if True:
-    #    MinVal = max(MinVal, 0.5)
-    #    MaxVal = min(MaxVal, 3.0)
-
-    print(f'MinVal is {MinVal}')
-    print(f'MaxVal is {MaxVal}')
-
-    #levels = [0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7, 1.9, 2.1, 2.3, 2.5, 2.7, 2.9]
-    MinMax = []
-    for i in range(len(levels)-1): 
+    #MinMax = [(-99999, levels[0])]
+    MinMax = [(disturbance.min(), levels[0])]
+    for i in np.arange(len(levels)-1): 
         MinMax.append((levels[i], levels[i+1]))
-    #MinMax.append((2.9, np.max(disturbance)))
-    print(f'MinMax is {MinVal}, {MaxVal}')
+    #MinMax.append((levels[-1], np.max(disturbance)))
 
     fig = plt.figure()
     ax = fig.add_subplot()
 
     my_cmap = plt.cm.jet
     contour = ax.tricontourf(triangulation, disturbance, vmin=MinVal, vmax=MaxVal,
-        levels=levels, cmap=my_cmap, extend='neither')
+        levels=levels, cmap=my_cmap, extend='min')
 
     #Transform a `matplotlib.contour.QuadContourSet` to a GeoDataFrame
     polygons, colors = [], []
     data = []
     for i, polygon in enumerate(contour.collections):
         mpoly = []
-        print(f'polygon {i}')
         for path in polygon.get_paths():
             try:
                 path.should_simplify = False
@@ -77,10 +75,10 @@ def contour_to_gdf(disturbance, levels, triangulation):
                 exterior, holes = [], []
                 if len(poly) > 0 and len(poly[0]) > 3:
                     # The first of the list is the exterior ring :
-                    exterior = np.round(poly[0], 5)
+                    exterior = poly[0]
                     # Other(s) are hole(s):
                     if len(poly) > 1:
-                        holes = [np.round(h, 5) for h in poly[1:] if len(h) > 3]
+                        holes = [h for h in poly[1:] if len(h) > 3]
                 mpoly.append(make_valid(Polygon(exterior, holes)))
             except:
                 print('Warning: Geometry error when making polygon #{}'.format(i))
@@ -111,7 +109,7 @@ def contour_to_gdf(disturbance, levels, triangulation):
     gdf['rgba'] = colors_elev
 
     #set crs
-    gdf = gdf.set_crs('epsg: 4326')
+    gdf = gdf.set_crs(4326)
 
     return  gdf
 
@@ -158,32 +156,25 @@ if __name__ == "__main__":
     idxs = np.argmax(elev, axis=0)
     time_maxelev = times[idxs]
 
-    filename_output = f'./stofs3d_atlantic_max_disturbance_{dates[0].strftime("%Y%m%d")}'  \
-        + f't01z_{dates[-1].strftime("%Y%m%d")}t00z.json'
+    #filename_output = f'./stofs3d_atlantic_max_disturbance_{dates[0].strftime("%Y%m%d")}'  \
+    #    + f't01z_{dates[-1].strftime("%Y%m%d")}t00z.shp'
 
-    t0 = time()
-    #levels = [0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7, 1.9, 2.1, 2.3, 2.5, 2.7, 2.9]
-    levels = [round(0.5 + i*0.2, 1 ) for i in range(13)]
-    levels.append(10)
-    print(levels)
-    get_disturbance(maxelev, depth, levels, my_fillvalue, filename_output)
-    print(f'Calculating and masking disturbance took {time()-t0} seconds')
+    #t0 = time()
+    levels = [round(i, 1) for i in np.arange(0.3, 2.1, 0.1)]
+    #print(levels)
+    #get_disturbance(maxelev, depth, levels, my_fillvalue, filename_output)
+    #print(f'Calculating and masking disturbance took {time()-t0} seconds')
 
 
     #calculate hourly disturbance
     npool = len(times) if len(times) < mp.cpu_count() else mp.cpu_count()-1
-    print(npool)
+    #print(npool)
 
     filenames = [f'./stofs3d_atlantic_disturbance_{dates[0].strftime("%Y%m%d")}' \
-        + f't00z_{dates[i].strftime("%Y%m%d")}t{dates[i].strftime("%H")}z.json' for i in range(len(times))]
-    print(filenames)
+        + f't00z_{dates[i].strftime("%Y%m%d")}t{dates[i].strftime("%H")}z.gpkg' for i in range(len(times))]
+    #print(filenames)
 
     t0 =  time()
-    #for it in range(len(times)):
-    #    print(it)
-    #    get_disturbance(np.squeeze(elev[it,:]), depth, levels, my_fillvalue, filenames[it])
-
-    #print(f'Calculating and masking disturbance for all times took {time()-t0} seconds')
 
     pool = mp.Pool(npool)
     pool.starmap(get_disturbance, [(np.squeeze(elev[i,:]), depth, levels, my_fillvalue, filenames[i]) for i in range(len(times))])
